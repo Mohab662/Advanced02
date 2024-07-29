22562dca273194361f759c4e47d408082485a2e4
Mohab662 <mohabmbelkan@gmail.com>
2024-07-29 18:32:54 +0300

    Add project files.

---
+++ Advanced02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Advanced02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+
+            #endregion
+        }
+    }
+}
